31b9cf294b51f9d260f628feca810ac3ad569b09
yoon-seul <66136653+yoon-seul@users.noreply.github.com>
2021-07-04 17:30:06 +0900

    Using 2D Arrays

--- play_hanoi.c
+++ play_hanoi.c
@@ -5,47 +5,63 @@
 
 int main(){	
 	int disk;
-	int hanoiA[9]; //Hanoi탑의 축
-	int hanoiB[9] = { ' ' };
-	int hanoiC[9] = { ' ' };
-	char input[3] = {'0','0','0'}; 
+	int hanoi[3][9];
+	char input[3] = { 0,0,0 };
 	int moveDisk;
+
+	for (int i = 0; i < 9; i++) {
+		hanoi[0][i] = i + 1;
+		hanoi[1][i] = 0;
+		hanoi[2][i] = 0;
+	}
 
 	printf("Insert the number of disks(1~9): ");
 	scanf_s("%d", &disk);
 
-	for (int i = 0; i < disk; i++) {
-		hanoiA[i] = i + 1;	//Hanoi탑 A축에 disk 넣기
-	}
-
 	while (input[0] != 'q') {
 
 		printf("%3c %3c %3c\n", 'A', 'B', 'C');
-
-		print_stack(hanoiA);
-
-		scanf_s("%c", &input); //disk 이동 받기
+		for (int i = 0; i < disk; i++) {
+			printf("%3d %3d %3d\n", hanoi[0][i], hanoi[1][i], hanoi[2][i]);
+		}
+			scanf_s("%s", &input); //disk 이동 받기
 
 		if (input[0] == 'q') {
 			return 0;
 		}
 		else {
-			if (input[0] == 'A' && input[2] == 'B') {
-				for (int i = 0; peek(hanoiA[i] != ' '); i++) {
-					moveDisk = peek(hanoiA[i + 1]);
-					pop(hanoiA[i + 1]);
-					if (hanoiB[0] == ' ') {
-						push(hanoiB[0], moveDisk);
-					}
-					for (int i = 0; peek(hanoiB[i] != ' '); i++) {
-						push(hanoiB[i+1], moveDisk);
-					}
-					
+			if (input[0] == 'A') {
+				for (int i = 0; peek(hanoi[0][i])!='\0'; i++) {
+					moveDisk = peek(hanoi[0][i]);
 				}
 			}
-
+			if (input[0] == 'B') {
+				for (int i = 0; peek(hanoi[1][i]) != '\0'; i++) {
+					moveDisk = peek(hanoi[1][i]);
+				}
+			}
+			if (input[0] == 'C') {
+				for (int i = 0; peek(hanoi[2][i]) != '\0'; i++) {
+					moveDisk = peek(hanoi[2][i]);
+				}
+			}
+			if (input[1] == 'A') {
+				for (int i = 0; peek(hanoi[0][i]) != '\0'; i++) {
+					hanoi[0][i] = moveDisk;
+				}
+			}
+			if (input[1] == 'B') {
+				for (int i = 0; peek(hanoi[1][i]) != '\0'; i++) {
+					hanoi[1][i] = moveDisk;
+				}
+			}
+			if (input[1] == 'C') {
+				for (int i = 0; peek(hanoi[2][i]) != '\0'; i++) {
+					hanoi[2][i] = moveDisk;
+				}
+			}
 		}
-		
 	}
+		
 	return 0;
 }
